cdea3b309b7d228df4b5cc3cc6854ec6872c6759
Darthseid <jared.mcvey@yahoo.com>
2015-12-21 16:14:36 -0600

    Update kelen_criminal_scum.lua

--- scripts/vscripts/kelen_criminal_scum.lua
+++ scripts/vscripts/kelen_criminal_scum.lua
@@ -1,15 +1,27 @@
 function Criminal( keys )
+		print ("hi")
 local caster = keys.caster
 local target = keys.target
-local targetLocation = target:GetAbsOrigin()
-local bonus_gold = ability:GetLevelSpecialValueFor("bonus_gold", (ability:GetLevel() - 1))
-local bonus_gold_radius = ability:GetLevelSpecialValueFor("bonus_gold_radius", (ability:GetLevel() - 1))
-local bonus_gold_targets = FindUnitsInRadius(caster:GetTeam() , targetLocation, nil, bonus_gold_radius, DOTA_UNIT_TARGET_TEAM_ENEMY , DOTA_UNIT_TARGET_HERO, 0, 0, false)
+local ability = keys.ability
+local bonus_gold = 250
+		print(bonus_gold)
+local targetLocation = caster:GetAbsOrigin()
+local bonus_gold_radius = 815
 
-bonus_gold_targets:ModifyGold(bonus_gold, true, 0)
+		print(bonus_gold_radius)
+local bonus_gold_targets = FindUnitsInRadius(caster:GetTeam() , targetLocation, nil, bonus_gold_radius, DOTA_UNIT_TARGET_TEAM_ENEMY , DOTA_UNIT_TARGET_HERO, 0, 0, false)
+		print (bonus_gold_targets)
+
+	for k,v in pairs(bonus_gold_targets) do v:ModifyGold(bonus_gold, true, 0)
+	end
+		print("Next Up")
 end
 
 function Scum( keys )
 local caster = keys.caster
-caster:ModifyGold(gold_loss, true, 0)
+local ability = keys.ability
+local gold_loss = -500
+		print(gold_loss)
+	caster:ModifyGold(gold_loss, true, 0)
+		print("bye")
 end
