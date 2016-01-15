00a317417ebfb9091c8d0aa5412c1e4866d72da9
Darthseid <jared.mcvey@yahoo.com>
2016-01-14 21:34:55 -0600

    Now Swap actually does something.

--- scripts/vscripts/item_steal_function.lua
+++ scripts/vscripts/item_steal_function.lua
@@ -1,46 +1,46 @@
-function Five( keys )
+function Swap( keys )
 	local caster = keys.caster
 	local target = keys.target
 	local ability = keys.ability
 	
-	
-target:GetNumItemsinInventory()
+  if target:GetNumItemsInInventory() < 6 then
+  
+    local demItems = {}
+    for itemSlot = 0, 5 do 
+      local item = target:GetItemInSlot(itemSlot)
+      if item then
+        table.insert(demItems, item)
+      end
+      print("INVENTORY IS COOL")
+    end
 
-GetNumItemsinInventory = v
-
-for itemSlot =  RandomInt( int 0, int v ) , = i, 1 do
-local item = target:GetItemInSlot(itemSlot)
- if item ~= nil then
- local itemName = itemGetName()
- itemName:GetCurrentCharges = z
- local newItem = CreateItem(itemName, caster, target)
- 
-itemName:SetCurrentCharges(z)
-caster:AddItem(itemName)
-target:RemoveItem(itemName)
-
-itemName:IsDroppable = false
-itemName:IsSellable = false
-itemName:IsDisassemblable = false
-itemName:IsCombinable = false
-
+		if #demItems > 0 then
+    	local randomItem = demItems[RandomInt(1,#demItems)]
+    	local theItem = target:Script_TakeItem(randomItem)		
+		print ("Grey Fox")
+		theItem:SetOwner(nil)
+		theItem:SetPurchaser(nil)
+    	caster:AddItem(theItem)
+		end
+	else
+  	print("INVENTORY IS FOOL")
+  end
 end
 
-function Red( keys )
+function Drop( keys )
 local caster = keys.caster
 	local target = keys.target
 	local ability = keys.ability
-	
-if caster:HasItemInInventory(itemName) then
-local item = caster:GetItemInSlot(itemSlot)
- if item ~= nil then
- local itemName = itemGetName()
-  itemName:GetCurrentCharges = z
- local newItem = CreateItem(itemName, caster, target)
- 
-caster:RemoveItem(itemName)
-itemName:SetCurrentCharges(z)
-target:AddItem(itemName)
-						end
-				end
-			end
+	local itemName = tostring(keys.ability:GetAbilityName())
+for itemSlot = 0, 5 do 
+      local item = target:GetItemInSlot(itemSlot)
+      if item then
+local theItem = caster:Script_TakeItem(stolenItem)		
+		print ("Grey Fox")
+		theItem:SetOwner(nil)
+		theItem:SetPurchaser(nil)
+    	target:AddItem(theItem)
+		end
+	else
+  	print("INVENTORY IS FOOL")
+  end
