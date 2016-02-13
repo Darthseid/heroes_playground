0979b1fbb990c872b42167051e283545db88d0a2
Darthseid <jared.mcvey@yahoo.com>
2016-02-13 13:57:00 -0600

    Simplified and actually working.

--- scripts/vscripts/mug_function_gold_lua.lua
+++ scripts/vscripts/mug_function_gold_lua.lua
@@ -1,29 +1,21 @@
-function mug_attack_loss( keys )
+function mug_attack_steal( keys )
 
     local attacker = keys.attacker
     local target = keys.target
     local ability = keys.ability
 
-    ability.gold_steal_percent = ability:GetLevelSpecialValueFor("gold_loss_percent", ability:GetLevel() - 1)
-    local mug_modifier = keys.mug_modifier 
-	
+    ability.gold_loss_percent = ability:GetLevelSpecialValueFor("gold_loss_percent", ability:GetLevel() - 1)
+ability.gold_gain_percent = ability:GetLevelSpecialValueFor("gold_gain_percent", ability:GetLevel() - 1)
+
+local Damage = attacker:GetAttackDamage()
+
 if keys.target:IsHero() and keys.target:IsOpposingTeam(keys.caster:GetTeam()) then
- target:ModifyGold = target:GetDamageDoneToHero() * (ability.gold_loss_percent / 100)
+local take = Damage * (ability.gold_loss_percent / 100)
+local get = Damage * (ability.gold_gain_percent / 100)
 
-    -- this sets the number of stacks of damage
-    ability:ApplyDataDrivenModifier(attacker, attacker, mug_modifier, {})
-    attacker:SetModifierStackCount(mug_modifier, ability, damage)
+target:ModifyGold(take, false, 0)
+attacker:ModifyGold(get, false, 0)
+	end
 end
 
-function mug_attack_gain( keys )
-  local attacker = keys.attacker
-  local target = keys.target
-  local ability = keys.ability
 
-  ability.gold_gain_percent = ability:GetLevelSpecialValueFor("gold_gain_percent", ability:GetLevel() - 1)
-  if keys.target:IsHero() and keys.target:IsOpposingTeam(keys.caster:GetTeam()) then
-  
-  attacker:ModifyGold = target:GetDamageDoneToHero() * (ability.gold_gain_percent / 100)
-
- 
-end
